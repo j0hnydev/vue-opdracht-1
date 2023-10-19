8a931458947dcc189ca5a436e167c431f0bfeeb9
Naoufal <43816561+NaoufalMaazouziPXL@users.noreply.github.com>
2023-10-19 22:03:50 +0200

    Finished assignment

---
+++ voorbeeld/src/App.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+  data() {
+    return {
+      product: '',
+      products: []
+    }
+  },
+  methods: {
+    addProduct() {
+      this.products.push(this.product)
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <label for="product">Product</label>
+    <input type="text" id="product" v-model="product" />
+    <button v-on:click="addProduct">Voeg dit product toe</button>
+  </div>
+  <ul>
+    <li v-for="produc in products">
+      {{ product }}
+    </li>
+  </ul>
+</template>
+
+<style>
+input,
+label {
+  margin-bottom: 0.5rem;
+  display: block;
+  width: 100%;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  margin: 3rem auto;
+  max-width: 40rem;
+  padding: 1rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+}
+
+label {
+  font-weight: bold;
+}
+
+ul {
+  list-style: none;
+  margin: 1rem 0;
+  padding: 0;
+}
+
+li {
+  margin: 1rem 0;
+  padding: 1rem;
+  border: 1px solid #ccc;
+}
+</style>
